fa7e75debab842a3fc981f84def4f5e4780b2a97
Daniel Dunbar <dunbardanielj@gmail.com>
2021-07-30 02:01:09 -0700

    Improve styling of dialog image

--- src/app/gallery/image-dialog/image-dialog.component.ts
+++ src/app/gallery/image-dialog/image-dialog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { saveAs } from 'file-saver';
 import { Photo } from 'pexels';
+import { CurrentViewService } from 'app/common/current-view/current-view.service';
+import { SupportedViews } from 'app/common/current-view/supported-views-enum';
 
 export interface ImageDialogData {
   photo: Photo;
@@ -13,19 +15,35 @@ export interface ImageDialogData {
   styleUrls: ['./image-dialog.component.sass'],
 })
 export class ImageDialogComponent implements OnInit {
+  photoUrl = '';
+
   constructor(
+    readonly currentViewService: CurrentViewService,
     readonly dialogRef: MatDialogRef<ImageDialogComponent>,
     @Inject(MAT_DIALOG_DATA) readonly data: ImageDialogData
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentViewService.currentView.subscribe((view) => {
+      switch (view) {
+        case SupportedViews.DESKTOP:
+          this.photoUrl = this.data.photo.src.large;
+          break;
+        case SupportedViews.TABLET:
+          this.photoUrl = this.data.photo.src.medium;
+          break;
+        default:
+          this.photoUrl = this.data.photo.src.medium;
+      }
+    });
+  }
 
   closeDialog() {
     this.dialogRef.close();
   }
 
   downloadImage() {
-    saveAs(this.data.photo.src.large, this.data.photo.photographer + '.png');
+    saveAs(this.photoUrl, this.data.photo.photographer + '.png');
     this.dialogRef.close();
   }
 }
